220b7bc80c802aee27ae213b0695ef509e9de261
CDH <rhdehgks@naver.com>
2023-01-24 02:29:52 +0900

    Add Dependencies

---
+++ src/main/java/com/dohan/doj/entity/Member.java
@@ -0,0 +1,15 @@
+package com.dohan.doj.entity;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="member")
+@Getter @Setter
+
+public class Member {
+}
