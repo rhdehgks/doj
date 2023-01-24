e22b6d82567f4c614504441d5dddb3b5f079f238
CDH <rhdehgks@naver.com>
2023-01-24 15:59:32 +0900

    Add Member Entity

---
+++ src/main/java/com/dohan/doj/constant/MemberRole.java
@@ -0,0 +1,5 @@
+package com.dohan.doj.constant;
+
+public enum MemberRole {
+    USER, ADMIN
+}
--- src/main/java/com/dohan/doj/entity/Member.java
+++ src/main/java/com/dohan/doj/entity/Member.java
@@ -1,15 +1,34 @@
 package com.dohan.doj.entity;
 
 
+import com.dohan.doj.constant.MemberRole;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name="member")
-@Getter @Setter
-
+@Getter
 public class Member {
+
+    @Id
+    @Column(name="id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 기본 키 생성방식. DB에 의존하지 않으려면 AUTO로 하는 방법도 있다.
+    private int id;
+
+    @Column(name="nickname")
+    @Setter
+    private String nickname;
+
+    @Column(name="email")
+    private String email;
+
+    @Column(name="password")
+    @Setter
+    private String password;
+
+    @Column(name="member_role")
+    @Setter
+    private MemberRole memberRole;
 }
